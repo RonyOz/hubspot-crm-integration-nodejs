e4cae5528225ae9ff68019643fe714a4f1c0dc3c
Rony Ordoñez <ronyfarido@gmail.com>
2026-09-13 21:09:59 -0500

    fix: set default value for stageId in config to 'appointmentscheduled'

--- src/config/env.js
+++ src/config/env.js
@@ -16,7 +16,7 @@ const config = {
     accessToken: requireEnv('HUBSPOT_ACCESS_TOKEN'),
     portalId: process.env.HUBSPOT_PORTAL_ID || null,
     pipelineId: process.env.HUBSPOT_PIPELINE_ID || 'default',
-    stageId: process.env.HUBSPOT_STAGE_ID || null,
+    stageId: process.env.HUBSPOT_STAGE_ID || 'appointmentscheduled',
     maxRetries: Number(process.env.HUBSPOT_MAX_RETRIES || 3),
     retryBaseDelayMs: Number(process.env.HUBSPOT_RETRY_BASE_DELAY_MS || 500),
   },
